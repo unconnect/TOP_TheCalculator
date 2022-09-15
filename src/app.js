e61eba2d7246e5db7dde8509864e4fcb47f15c69
Alexander Nikolas Reuber <info@unconnect.net>
2022-09-15 20:48:27 +0200

    added display update functionality and clearing

--- src/app.js
+++ src/app.js
@@ -1,44 +1,76 @@
+const elements = {
+  command_keys: document.querySelectorAll(".command_keys button"),
+  operator_keys: document.querySelectorAll(".operator_keys button"),
+  number_keys: document.querySelectorAll(".number_keys button"),
+  display: document.querySelector(".display .output")
+};
+
+const data = {
+  clear: true,
+  values: [],
+  operator: null,
+};
+
+// Mathematical Operations
 const add = (val1, val2) => {
-  return val1 + val2
-}
+  return val1 + val2;
+};
 
 const substract = (val1, val2) => {
-  return val1 - val2
-}
+  return val1 - val2;
+};
 
 const multiply = (val1, val2) => {
   return val1 * val2;
 };
 
 const divide = (val1, val2) => {
-  return val1 / val2
-}
+  return val1 / val2;
+};
 
 const operate = (val1, val2, operant) => {
   switch (operant) {
-    case '+':
-      return add(val1, val2)
-    case '-':
+    case "+":
+      return add(val1, val2);
+    case "-":
       return substract(val1, val2);
-    case '*':
+    case "*":
       return multiply(val1, val2);
-    case '/':
+    case "/":
       return divide(val1, val2);
     default:
       return false;
   }
-} 
+};
 
-console.table({
-  "add 1 + 2": add(1, 2),
-  "substract 1 - 2": substract(1, 2),
-  "multiply 1 * 2": multiply(1, 2),
-  "divide 1 / 2": divide(1, 2),
-  "divide 0 / 1": divide(0, 1),
-  "divide 1 / 0": divide(1, 0),
-  "operate 1 + 2": operate(1, 2, "+"),
-  "operate 1 - 2": operate(1, 2, "-"),
-  "operate 1 * 2": operate(1, 2, "*"),
-  "operate 1 / 2": operate(1, 2, "/"),
-});
+const updateDisplay = (value) => {
+  if(elements.display.textContent == '0') elements.display.textContent = "";
+  elements.display.textContent += value;
+}
 
+const updateCalculator = (e) => {
+  if(e.target)
+  console.log(e.target.value)
+  updateDisplay(e.target.value)
+}
+
+const clearCalculator = () => {
+  data['clear'] = true
+  data['values'] = []
+  data['operator'] = null
+  elements.display.textContent = 0
+}
+
+const init = () => {
+  elements.number_keys.forEach(key => {
+    key.addEventListener('click', updateCalculator, false)
+  })
+  elements.command_keys.forEach((key) => {
+    key.addEventListener("click", clearCalculator, false);
+  });
+};
+
+init();
+
+// Debug Output
+console.log(elements);
