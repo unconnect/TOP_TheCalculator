bc38156a6f0ec64d5fce4ce539821e1dbeaab1ad
Alexander Nikolas Reuber <info@unconnect.net>
2022-09-20 21:19:09 +0200

    refactored operate, updateDisplay, updateOperant and setOperation, seperated euqal operation from other operators

--- src/app.js
+++ src/app.js
@@ -1,137 +1,144 @@
 const ui = {
   numberKeys: document.querySelectorAll("[data-key]"),
   operatorKeys: document.querySelectorAll("[data-operator]"),
+  equalKey: document.querySelector('[data-equal]'),
   currentOperantText: document.querySelector("[data-current_operant_output]"),
   previousOperantText: document.querySelector("[data-previous_operant_output]"),
   operatorText: document.querySelector("[data-operator_output]"),
   clearKey: document.querySelector("[data-all-clear]"),
   deleteKey: document.querySelector("[data-delete]"),
-};
+}
 
 const calcData = {
   clear: true,
   currentOperant: "",
   previousOperant: "",
   result: "",
-  calculationOperator: "",
-  currentOperator: "",
-};
+  operator: "",
+}
 
 // Mathematical Operations
 const add = (val1, val2) => {
-  return String(parseFloat(val1) + parseFloat(val2));
-};
-
-const substract = (val1, val2) => {
-  return String(parseFloat(val1) - parseFloat(val2));
-};
-
-const multiply = (val1, val2) => {
-  return String(parseFloat(val1) * parseFloat(val2));
-};
-
-const divide = (val1, val2) => {
-  return String(parseFloat(val1) / parseFloat(val2));
-};
-
-const operate = (data) => {
-  switch (data.calculationOperator) {
-    case "+":
-      data.result = add(data.previousOperant, data.currentOperant);
-      break;
-    case "-":
-      data.result = substract(data.previousOperant, data.currentOperant);
-      break;
-    case "*":
-      data.result = multiply(data.previousOperant, data.currentOperant);
-      break;
-    case "/":
-      data.result = divide(data.previousOperant, data.currentOperant);
-      break;
-  }
-  setResult(calcData);
-};
-
-const setResult = (data) => {
-    if (data.currentOperator === "=") {
-      data.previousOperant = "";
-      data.currentOperant = data.result;
-      updateDisplay(data);
-      data.calculationOperator = "";
-      return;
-    }
-    data.previousOperant = data.result;
-    data.calculationOperator = data.currentOperator;
-    data.currentOperant = "";
-    updateDisplay(data);
+  return String(val1 + val2)
 }
 
-const updateDisplay = (data) => {
-  ui.currentOperantText.textContent = data.currentOperant
-  ui.previousOperantText.textContent = data.previousOperant;
-  ui.operatorText.textContent = data.currentOperator;
-  console.table(data);
+const substract = (val1, val2) => {
+  return String(val1 - val2)
+}
+
+const multiply = (val1, val2) => {
+  return String(val1 * val2)
+}
+
+const divide = (val1, val2) => {
+  return String(val1 / val2)
+}
+
+const operate = () => {
+  let result
+  const previousOperant = parseFloat(calcData.previousOperant)
+  const currentOperant = parseFloat(calcData.currentOperant)
+  if(isNaN(previousOperant) || isNaN(currentOperant)) return
+  switch (calcData.operator) {
+    case "+":
+      result = add(previousOperant, currentOperant)
+      break
+    case "-":
+      result = substract(previousOperant, currentOperant)
+      break
+    case "*":
+      result = multiply(previousOperant, currentOperant)
+      break
+    case "/":
+      result = divide(previousOperant, currentOperant)
+      break
+    default:
+      return
+  }
+  calcData.operator = undefined
+  calcData.currentOperant = result
+  calcData.previousOperant = ""
+}
+
+const updateDisplay = () => {
+  ui.currentOperantText.textContent = calcData.currentOperant
+  ui.previousOperantText.textContent = calcData.previousOperant
+  ui.operatorText.textContent = calcData.operator
+  console.table(calcData)
 }
 
 const updateCurrentOperant = (e) => {
-  if(calcData.clear || !calcData.currentOperant || calcData.result) {
-    calcData.result = ''
-    calcData.currentOperant = ''
+  if (calcData.clear || !calcData.currentOperant || calcData.result) {
+    calcData.result = ""
+    calcData.currentOperant = ""
     calcData.clear = false
   }
-  calcData.currentOperant = calcData.currentOperant + String(e.target.dataset.key);
-  updateDisplay(calcData)
+  calcData.currentOperant =
+    calcData.currentOperant + String(e.target.dataset.key)
 }
 
 const setOperation = (e) => {
-  const operator = String(e.target.dataset.operator);
-  if (operator === '=' && (!calcData.previousOperant || !calcData.calculationOperator || !calcData.currentOperant)) {
-    return;
-  }
-  if (operator && operator !== '=' && !calcData.currentOperant) {
-    calcData.currentOperator = operator;
-    calcData.calculationOperator = calcData.currentOperator;
-    updateDisplay(calcData);
-    return;
-  }
-  calcData.currentOperator = operator;
-  if (calcData.previousOperant && calcData.calculationOperator) {
-    operate(calcData);
-    return;
-  }
+  const operator = String(e.target.dataset.operator)
+  if (calcData.currentOperant === '') return
+  if (calcData.previousOperant !== '') operate()
+  calcData.operator = operator
   calcData.previousOperant = calcData.currentOperant
-  calcData.calculationOperator = calcData.currentOperator;
-  calcData.currentOperant = ''
-  updateDisplay(calcData)
+  calcData.currentOperant = ""
 }
 
 const clearCalculator = () => {
   calcData.clear = true
-  calcData.currentOperant = ''
-  calcData.previousOperant = ''
-  calcData.result = ''
-  calcData.calculationOperator = ''
-  calcData.currentOperator = ''
-  updateDisplay(calcData);
+  calcData.currentOperant = ""
+  calcData.previousOperant = ""
+  calcData.result = ""
+  calcData.operator = ""
 }
 
 const init = () => {
-  ui.numberKeys.forEach(key => {
-    key.addEventListener("click", updateCurrentOperant, false)
+  ui.numberKeys.forEach((key) => {
+    key.addEventListener(
+      "click",
+      (e) => {
+        updateCurrentOperant(e)
+        updateDisplay()
+      },
+      false
+    )
   })
-  ui.operatorKeys.forEach(key => {
-    key.addEventListener("click", setOperation, false)
+  ui.operatorKeys.forEach((key) => {
+    key.addEventListener(
+      "click",
+      (e) => {
+        setOperation(e)
+        updateDisplay()
+      },
+      false
+    )
   })
-  ui.clearKey.addEventListener("click", clearCalculator, false)
-};
+  ui.clearKey.addEventListener(
+    "click",
+    () => {
+      clearCalculator()
+      updateDisplay()
+    },
+    false
+  )
+  ui.equalKey.addEventListener("click", 
+  (e) => {
+    operate()
+    updateDisplay()
+  })
+}
 
-init();
+init()
 
 /**
  * TODOS
- * 
- * - [ ] round answers
+ *
  * - [x] BUGFIX: pressing = before entering all numbers causes still errors
+ * - [x] REFACTOR: calOperator seams redundant, should be removed
+ * - [ ] BUGFIX: equal Operator should have own eventlistener to just operate and updatedisplay or otherwise update setOperation fn with more logic
+ * - [ ] round answers
  * - [ ] Check for error and display message when trying to devide by 0
  * - [ ] EXTRA: make decimals . work
  * - [ ] EXTRA: make DEL button work
